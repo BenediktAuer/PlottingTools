38b3fb21e7a3a10ffd0b69b16191b1c0b7cdd645
BenediktAuer <benedikt_auer01@web.de>
2021-11-14 21:17:37 +0100

    modifidy tests

--- test.py
+++ test.py
@@ -17,6 +17,8 @@ strom = data[:, 2]
 strom_err = np.full(10, 0.5)
 strom_with_err = unumpy.uarray(strom,strom_err)
 spannung_with_err = unumpy.uarray(spannung,strom_err)
-test = Plottingtools.Messwerte(strom,strom_err,spannung,strom_err,"Strom", "Spannung")
+test = Plottingtools.Messwerte(strom,strom_err,spannung,strom_err,"Strom", "Spannung","Messung 1")
 print(test)
 test.errorbars("Spannung","UI-Diagramm")
+testlist = [test]
+Plottingtools.Messwerte.plots(testlist,"UI-Diagramm", "Strom", "Spannung")
